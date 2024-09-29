ccf79aece5f3a5f83ffcce76953726d77473f4e9
francisohara24 <franciskohara@gmail.com>
2024-09-29 11:56:42 -0400

    Feat: initial commit for homework 3 part 2 script

---
+++ Week 4/Homework 3 (Part 2).R
@@ -0,0 +1,8 @@
+######################################################################################
+# Filename:      Homework 3 (Part 2).R
+# Description:   R Scripts for SC212 Homework 3 (Part 2)
+# Author:        Francis O'Hara
+# Date Modified: 9/28/24
+######################################################################################
+data <- read.csv("./Week 4/data/fev.csv")
+data
