96976f091b0afa85231faf597263309c9c058e25
francisohara24 <franciskohara@gmail.com>
2024-10-04 00:08:38 -0400

    Feat: add Homework 3 part 2 script and plots.

--- Week 4/Homework 3 (Part 2).R
+++ Week 4/Homework 3 (Part 2).R
@@ -2,7 +2,20 @@
 # Filename:      Homework 3 (Part 2).R
 # Description:   R Scripts for SC212 Homework 3 (Part 2)
 # Author:        Francis O'Hara
-# Date Modified: 9/28/24
+# Date Modified: 10/04/24
 ######################################################################################
-data <- read.csv("./Week 4/data/fev.csv")
-data
+
+######################################################################################
+# Question 3.110
+######################################################################################
+library(Lock5Data)
+data(OttawaSenators2019)
+
+# create stacked dot plot
+stripchart(OttawaSenators2019$PenMins, method = "stack", offset = .5, at = 0,
+           pch = 19, col = "steelblue",
+           main = "Distribution of Penalty Minutes", xlab = "Minutes")
+
+# find mean and standard deviation
+mean(OttawaSenators2019$PenMins)
+sd(OttawaSenators2019$PenMins)
