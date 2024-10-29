b4ffad457fc24b441b5db8f85f79a6748191b144
francisohara24 <franciskohara@gmail.com>
2024-10-29 12:19:48 -0400

    Docs: update description

--- Week 8/Homework 5.R
+++ Week 8/Homework 5.R
@@ -1,5 +1,5 @@
 # Filename:      Homework 5.R
-# Description:   R Scripts for SC212 ...
+# Description:   R Scripts for SC212 Homework 5
 # Author:        Francis O'Hara
 # Date Modified: 10/25/24
 library(Lock5Data)
