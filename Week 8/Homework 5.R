389b69823aa33129a6c3d412daa3d69d7597d1f6
francisohara24 <franciskohara@gmail.com>
2024-10-29 12:20:44 -0400

    Docs: update description

--- Week 8/Homework 5.R
+++ Week 8/Homework 5.R
@@ -1,7 +1,9 @@
+######################################################################################
 # Filename:      Homework 5.R
 # Description:   R Scripts for SC212 Homework 5
 # Author:        Francis O'Hara
 # Date Modified: 10/25/24
+######################################################################################
 library(Lock5Data)
 data(MustangPrice)
 my.lm <- lm(MustangPrice$Price ~ MustangPrice$Miles)
