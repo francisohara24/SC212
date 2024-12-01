77c7313ebeb7e7cd9aad6a01725aede965db2a1b
francisohara24 <franciskohara@gmail.com>
2024-12-01 17:59:03 -0500

    Feat: update script for chapter 9 reading and add generated plots.

--- Week 12/Chapter 9.R
+++ Week 12/Chapter 9.R
@@ -7,5 +7,22 @@
 library(Lock5Data)
 data(InkjetPrinters)
 
+# compute regression line
 my.lm <- lm(InkjetPrinters$Price ~ InkjetPrinters$PPM)
 summary(my.lm)
+
+# plot data and regression line
+plot(InkjetPrinters$PPM, InkjetPrinters$Price,
+     xlab = "Pages Per Minute",
+     ylab = "Price",
+     main = "Regression Line (PPM vs Price)"
+)
+abline(my.lm,
+       col = "red")
+
+# compute 95% confidence interval for population slope of regression line
+t.star <- qt(0.975, df=length(InkjetPrinters$Price) - 2)
+sample.slope <- 90.88
+SE <-  19.49  # from regression output
+CI <- c(sample.slope - (t.star * SE), sample.slope + (t.star * SE))
+CI
